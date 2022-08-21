4734dc484a6c8864d69a7ae6c3d5886ce0ba16f0
tianiue <61725402+tianiue@users.noreply.github.com>
2022-08-21 18:15:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,7 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 git clone https://github.com/tianiue/luci-packages.git package/luci-packages
-git clone https://github.com/tianiue/openwrt-alist.git package/openwrt-alist
+git clone https://github.com/tianiue/luci-app-alist.git package/luci-app-alist
 # git clone https://github.com/tianiue/openwrt-passwall.git package/openwrt-passwall
 git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall-luci
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
