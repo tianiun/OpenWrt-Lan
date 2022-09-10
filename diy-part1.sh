0a6e70b44e7d186bec5ae5879458b6f868a9b0d4
tianiue <61725402+tianiue@users.noreply.github.com>
2022-09-10 22:28:26 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,7 +18,8 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 git clone https://github.com/tianiue/luci-packages.git package/luci-packages
 git clone https://github.com/tianiue/luci-app-alist.git package/luci-app-alist
-# git clone https://github.com/tianiue/openwrt-passwall.git package/openwrt-passwall
+git clone https://github.com/linkease/nas-packages-luci.git package/nas-packages-luci
+git clone https://github.com/linkease/nas-packages.git package/nas-packages
 git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall-luci
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
 git clone https://github.com/fw876/helloworld.git package/helloworld
@@ -26,8 +27,7 @@ git clone https://github.com/vernesong/OpenClash.git package/OpenClash
 git clone https://github.com/frainzy1477/luci-app-clash.git package/luci-app-clash
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 # git clone https://github.com/jerrykuku/luci-app-ttnode.git package/luci-app-ttnode
-# git clone https://github.com/linkease/ddnsto-openwrt.git package/ddnsto-openwrt
 git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
-git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
-git clone https://github.com/lisaac/luci-lib-docker.git package/luci-lib-docker
+# git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
+# git clone https://github.com/lisaac/luci-lib-docker.git package/luci-lib-docker
